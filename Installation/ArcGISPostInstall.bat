4bee71634861a9b28524cb7a05034111e21e4f65
john.fay@duke.edu <john.fay@duke.edu>
2017-11-18 16:31:20 -0500

    Adds folium examples

---
+++ Installation/ArcGISPostInstall.bat
@@ -0,0 +1,15 @@
+pywin32-220.win32-py2.7.exe
+
+C:\python27\arcgis10.5\scripts\pip install --upgrade pip
+C:\python27\arcgis10.5\scripts\pip install jupyter
+C:\python27\arcgis10.5\scripts\pip install .\GDAL-2.2.2-cp27-cp27m-win32.whl
+C:\python27\arcgis10.5\scripts\pip install .\Fiona-1.7.9-cp27-cp27m-win32.whl
+C:\python27\arcgis10.5\scripts\pip install .\pyproj-1.9.5.1-cp27-cp27m-win32.whl
+C:\python27\arcgis10.5\scripts\pip install .\Rtree-0.8.3-cp27-cp27m-win32.whl
+C:\python27\arcgis10.5\scripts\pip install .\Shapely-1.6.1-cp27-cp27m-win32.whl
+
+set PATH=%PATH%;c:\\python27\\arcgis10.5\\lib\\site-packages\\osgeo
+
+C:\python27\arcgis10.5\scripts\pip install geopandas
+
+pause
